089711c9a92f878e9bf2759bdbfae8d346204a2c
hkRho <hrho@olin.edu>
2021-12-14 01:17:37 -0500

    Add randomize with instructions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # data-anonymizer
 Python tool to perform data anonymization on a MySQL database
+
+
+"anonymize_instructions.txt" describes what database, tables and what columns in the data to perform data anonymization on.
--- data_anonymizer.py
+++ data_anonymizer.py
@@ -1,5 +1,8 @@
 import pymysql
 import subprocess
+import numpy as np
+from faker import Faker
+
 
 class MySqlConnection:
     def __init__(self, db_host, db_user, db_pass, db_name):
@@ -7,6 +10,9 @@ class MySqlConnection:
         self.db_user = db_user
         self.db_pass = db_pass
         self.db_name = db_name
+
+        self.faker = Faker()
+        Faker.seed(0)
 
     def connectDB(self):
         # Connect to the database
@@ -28,25 +34,78 @@ class MySqlConnection:
         
         return MySqlConnection(self.db_host, self.db_user, self.db_pass, database_name)
 
+    def randomizeCategorical(self, table_name, column_name, faker_provider, where_clause=None):
+        connection = self.connectDB()
+        with connection.cursor() as cursor:
+            ## get data type of the original column
+            # cursor.execute(f"SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table_name}' AND COLUMN_NAME = '{column_name}';")
+            # data_type = cursor.fetchone()['DATA_TYPE']
 
-def group_by_where():
-    pass
+            ## find the number of distinct values of a given column
+            # cursor.execute(f'SELECT COUNT(DISTINCT {column_name}) FROM {table_name};')
+            # n = cursor.fetchone()['COUNT(DISTINCT name)']
 
-def get_update_table():
-    pass
+            # get all distinct values of a given column
+            cursor.execute(f'SELECT DISTINCT {column_name} FROM {table_name} {"WHERE " + where_clause if where_clause else ""};')
+            result = cursor.fetchall()
+            all_values = []
+            for value in result:
+                all_values.append(value[column_name])
 
-def anonymize_database(strategy):
-    # call get_update_table
+            # generate fake categorical values
+            fake_values = []
+            for i in range(len(all_values)):
+                fake_values.append(faker_provider())
+                cursor.execute(f'UPDATE {table_name} SET {column_name} = REPLACE({column_name}, "{all_values[i]}", "{fake_values[i]}")')
+        
+        # commit changes
+        connection.commit()
+        
+    def randomizeNumerical(self, table_name, column_name, preserve_distr=False):
+        connection = self.connectDB()
+        with connection.cursor() as cursor:
+            ## just generate one value and apply it everywhere
+            if preserve_distr:
+                fake_val = np.random.uniform(0, 1.1)
 
-    # apply that change on the database
-    pass
+                # get all distinct values of a given column
+                cursor.execute(f'SELECT DISTINCT {column_name} FROM {table_name};')
+                result = cursor.fetchall()
+                print(result)
+                all_values = []
+                for value in result:
+                    all_values.append(value[column_name])
+
+                # apply fake_val
+                for i in range(len(all_values)):
+                    cursor.execute(f'UPDATE {table_name} SET {column_name} = REPLACE({column_name}, "{all_values[i]}", "{all_values[i] + 10 * fake_val}")')
+                
+            ## generate different values for each distinct value
+            else:
+                # get all distinct values of a given column
+                cursor.execute(f'SELECT DISTINCT {column_name} FROM {table_name};')
+                result = cursor.fetchall()
+                all_values = []
+                for value in result:
+                    all_values.append(value[column_name])
+
+                # apply fake_val
+                for i in range(len(all_values)):
+                    cursor.execute(f'UPDATE {table_name} SET {column_name} = REPLACE({column_name}, "{all_values[i]}", "{all_values[i] + 10 * np.random.uniform(0, 1.1)}")')
 
 
-# connection = MySqlConnection('localhost', 'root', 'charizard', 'feedback')
+        # commit changes
+        connection.commit()
+
+
+## RUN BELOW ONLY ONCE
 # new_connection = connection.cloneDB('feedback2')
 
+# connection.randomizeCategorical('companies', 'name', connection.faker.company)
+connection.randomizeNumerical('votes', 'value')
+
 '''
-Output: Copy of database with specified tables anonymized through data masking
+Output: Copy of database with specified tables/rows anonymized through data masking
 TODO:
 - Connect to database with pymysql
 - Create copy of database
@@ -54,3 +113,28 @@ TODO:
 
 Then in demo show aggregation functions produce same result (distribution remains the same)
 '''
+
+if __name__ == "__main__":
+    lines = []
+    # Read from instruction file
+    with open('anonymize_instructions.txt', 'r') as f:
+        lines = f.readlines()
+    
+    # Get database, table, and columns to be anonymized
+    table_and_cols = {} # key: table name, value: list of columns in table
+    if len(lines) > 1:
+        lines = [line.rstrip() for line in lines]
+        db_name = lines[0]
+        for i in range(1, len(lines)):
+            names = lines[i].split(', ')
+            if len(names) > 1:
+                table_and_cols[names[0]] = names[1:]
+    else:
+        raise ValueError('anonymize_instructions.txt is empty. Use this file to detail what to anonymize')
+
+    db_pass = ''
+    with open('password.txt', 'r') as f:
+        db_pass = f.read()
+    conn = MySqlConnection('localhost', 'root', db_pass, db_name)
+    anon = conn.cloneDB(f'{db_name}_anonymized')
+    
